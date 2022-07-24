c68927761b9d05f71b1e393d0f602ea286c31f1e
jacklayfield <layf100@gmail.com>
2022-07-24 11:23:36 -0400

    Added packages, initialized wit.ai and wolfram components, initialized bot

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import("fmt", "context", "encoding/json", "log", "os", "github.com/joho/godotenv", "github.com/shomali11/slacker")
+
+func printCmdEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Cmd Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
+    godotenv.Load(".env")
     fmt.Println("Test, nothing yet")
+
+    bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+    go printCommandEvents(bot.CommandEvents())
 }
