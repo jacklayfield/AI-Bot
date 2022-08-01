19c154b0ec7604088d89b9acdd35977598138706
jacklayfield <layf100@gmail.com>
2022-07-31 21:45:41 -0400

    Got bot communicating with Slack, started connecting Wit.ai

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import("fmt", "context", "encoding/json", "log", "os", "github.com/joho/godotenv", "github.com/shomali11/slacker")
+import (
+	"context"
+	// "encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	// "github.com/krognol/go-wolfram"
+	"github.com/shomali11/slacker"
+	//"github.com/tidwall/gjson"
+
+	witai "github.com/wit-ai/wit-go/v2"
+
+)
 
 func printCmdEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
@@ -18,6 +32,31 @@ func main() {
     fmt.Println("Test, nothing yet")
 
     bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
 
-    go printCommandEvents(bot.CommandEvents())
+    go printCmdEvents(bot.CommandEvents())
+
+	bot.Command("query for bot - <message>", &slacker.CommandDefinition {
+		Description: "send query to Wolfram",
+		Example: "who is the the Pittsburgh Penguins captain",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			query := request.Param("message")
+
+			fmt.Println(query)
+			msg, _ := client.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			fmt.Println(msg)
+
+			response.Reply("Success")
+		}, 
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := bot.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
