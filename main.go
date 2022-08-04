9b6c279b296f780121d6d990eb11d5808f931016
jacklayfield <layf100@gmail.com>
2022-08-03 23:45:08 -0400

    Query parsing, now communicating with Wolfram client

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"context"
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
-	// "github.com/krognol/go-wolfram"
+	"github.com/krognol/go-wolfram"
 	"github.com/shomali11/slacker"
-	//"github.com/tidwall/gjson"
+	"github.com/tidwall/gjson"
 
 	witai "github.com/wit-ai/wit-go/v2"
 
@@ -31,14 +31,18 @@ func main() {
     godotenv.Load(".env")
     fmt.Println("Test, nothing yet")
 
+	//the slack bot itself
     bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	//the client from Wit.ai
 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+	//the Wolfram client
+	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
 
     go printCmdEvents(bot.CommandEvents())
 
 	bot.Command("query for bot - <message>", &slacker.CommandDefinition {
 		Description: "send query to Wolfram",
-		Example: "who is the the Pittsburgh Penguins captain",
+		Example: "who is the CEO of Alphabet",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			query := request.Param("message")
 
@@ -46,7 +50,21 @@ func main() {
 			msg, _ := client.Parse(&witai.MessageRequest{
 				Query: query,
 			})
-			fmt.Println(msg)
+			//get json
+			data, _ := json.MarshalIndent(msg, "", "    ")
+			//get string
+			rough := string(data[:])
+			//get the actual query to send to wolfram
+			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value") 
+			//convert query to expected string value to send to wolfram
+			answer := value.String()
+			//get the result from wolfram
+			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println("there is an error")
+			}
+			fmt.Println(value)
+			response.Reply(res)
 
 			response.Reply("Success")
 		}, 
